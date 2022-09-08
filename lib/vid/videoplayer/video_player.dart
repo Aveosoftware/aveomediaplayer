f3b1768bf018fa05c6bb232f1f05bb164a3a7c15
devalchauhan <er.devalchauhan@gmail.com>
2022-09-08 13:13:07 +0530

    Default controllers added for video player

--- lib/vid/videoplayer/video_player.dart
+++ lib/vid/videoplayer/video_player.dart
@@ -290,38 +290,43 @@ class _VideoStackState extends State<VideoStack>
               widget.videoPlayerController,
               key: widget.playerKey,
             ),
-            FadeTransition(
-              opacity: animation,
-              child: Positioned(
-                top: 0,
-                child: FractionallySizedBox(
-                  widthFactor: 1,
-                  heightFactor: .2,
-                  child:
-                      widget.topActions?.call(widget.videoPlayerController) ??
-                          const SizedBox.shrink(),
-                ),
-              ),
-            ),
-            FadeTransition(
-              opacity: animation,
-              child: Positioned(
-                bottom: 0,
-                child: FractionallySizedBox(
-                  widthFactor: 1,
-                  heightFactor: .3,
-                  child: widget.bottomActions
-                          ?.call(widget.videoPlayerController) ??
-                      const SizedBox.shrink(),
-                ),
-              ),
-            ),
             GestureDetector(
               behavior: HitTestBehavior.translucent,
               onTap: () {
                 log('top');
                 showControls.value = !showControls.value;
               },
+            ),
+            Positioned(
+              top: 0,
+              child: FadeTransition(
+                opacity: animation,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  // height:
+                  //     (widget.aspectRatio * MediaQuery.of(context).size.width) *
+                  //         .2,
+                  child:
+                      widget.topActions?.call(widget.videoPlayerController) ??
+                          const SizedBox.shrink(),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: FadeTransition(
+                opacity: animation,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  // height: double.infinity * .3,
+                  // (widget.aspectRatio * MediaQuery.of(context).size.width) *
+                  //     .7,
+                  child: widget.bottomActions
+                          ?.call(widget.videoPlayerController) ??
+                      DefaultBottomControls(
+                          controller: widget.videoPlayerController),
+                ),
+              ),
             ),
           ],
         ),
@@ -437,9 +442,128 @@ class DefaultBottomControls extends StatefulWidget {
 }
 
 class _DefaultBottomControlsState extends State<DefaultBottomControls> {
+  double sliderVal = 0.0;
+
+  sliderValSetter() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (widget.controller.value.isPlaying && mounted) {
+        setState(() {
+          sliderVal = ((widget.controller.value.position.inSeconds /
+                  widget.controller.value.duration.inSeconds) *
+              100);
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    sliderValSetter();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  onPressed: () => showModalBottomSheet(
+                      context: context,
+                      builder: (_) => PlaybackSpeedVimeo(
+                            controller: widget.controller,
+                          ),
+                      backgroundColor: Colors.white),
+                  icon: const Icon(
+                    Icons.speed,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () => widget.controller.seekTo(Duration(
+                      seconds:
+                          widget.controller.value.position.inSeconds - 10)),
+                  icon: const Icon(Icons.replay_10, color: Colors.white)),
+              IconButton(
+                  onPressed: () {
+                    widget.controller.value.isPlaying
+                        ? widget.controller.pause()
+                        : widget.controller.play();
+                    setState(() {});
+                  },
+                  icon: widget.controller.value.isPlaying
+                      ? const Icon(Icons.pause, color: Colors.white)
+                      : const Icon(Icons.play_arrow, color: Colors.white)),
+              IconButton(
+                  onPressed: () => widget.controller.seekTo(Duration(
+                      seconds:
+                          widget.controller.value.position.inSeconds + 10)),
+                  icon: const Icon(Icons.forward_10, color: Colors.white)),
+              IconButton(
+                  onPressed: () => widget.controller.toggleFullScreen(context),
+                  icon: widget.controller.isFullScreen().value
+                      ? const Icon(
+                          Icons.fullscreen_exit,
+                          color: Colors.white,
+                        )
+                      : const Icon(Icons.fullscreen, color: Colors.white)),
+            ]),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              printDuration(widget.controller.value.position),
+              style: progressStyle,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: SliderTheme(
+                data: SliderThemeData(
+                    trackShape: CustomTrackShape(),
+                    overlayShape: SliderComponentShape.noThumb,
+                    thumbShape: const RoundSliderThumbShape(
+                        pressedElevation: 0, enabledThumbRadius: 5),
+                    trackHeight: 2),
+                child: Slider(
+                  min: 0,
+                  max: 100,
+                  // divisions: 100,
+                  value: sliderVal.toDouble(),
+                  thumbColor: Color(0xffe9e9e9),
+                  activeColor: Color(0xffe9e9e9),
+                  inactiveColor: Color(0xff3E3D3D),
+                  onChanged: (double value) {
+                    setState(() {
+                      sliderVal = value;
+                    });
+                    widget.controller.seekTo(Duration(
+                        seconds: ((value / 100) *
+                                widget.controller.value.duration.inSeconds)
+                            .toInt()));
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              printDuration(widget.controller.value.duration),
+              style: progressStyle,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+      ],
+    );
   }
 }
 
